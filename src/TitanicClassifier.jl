0d4d8bfd4c2b6d6ad463947d8880c7b6acabb965
Černý, Adam <cernyad3@fel.cvut.cz>
2023-01-21 17:50:35 +0100

    initial commit

--- src/TitanicClassifier.jl
+++ src/TitanicClassifier.jl
@@ -1,5 +1,11 @@
 module TitanicClassifier
 
-# Write your package code here.
+export greet
+export greet2
+export greet3
+
+greet() = println("Hello World!")
+greet2() = println("Hello Boy!")
+greet3() = println("Hello Girl")
 
 end
